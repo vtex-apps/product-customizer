137a31aac7377138c017b1cbe4d956fb48b419f8
Christian Kaisermann <christian@kaisermann.me>
2020-11-12 16:48:27 -0300

    Add window.matchMedia mock

---
+++ react/setupTests.js
@@ -0,0 +1,14 @@
+// Necessary, otherwise vtex.styleguide break our tests
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
