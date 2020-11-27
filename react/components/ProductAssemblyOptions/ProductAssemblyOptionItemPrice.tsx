d27a7e2bf9b52b9568320d7aeed3133b2972afaf
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2020-11-27 19:43:29 -0300

    Add classes on product price

--- react/components/ProductAssemblyOptions/ProductAssemblyOptionItemPrice.tsx
+++ react/components/ProductAssemblyOptions/ProductAssemblyOptionItemPrice.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
 import ProductPrice from 'vtex.store-components/ProductPrice'
 
 import { useProductAssemblyItem } from '../ProductAssemblyContext/Item'
 import { withItem } from './withItem'
+
+const CSS_HANDLES = [
+  'productAssemblyOptionItemPrice',
+  'productAssemblyOptionItemSign',
+] as const
 
 const sumAssembliesPrice = (
   assemblyOptions: Record<string, AssemblyOptionGroupState>
@@ -15,6 +21,7 @@ const sumAssembliesPrice = (
         const childrenPrice = item.children
           ? sumAssembliesPrice(item.children)
           : 0
+
         return groupSum + (item.price + childrenPrice) * item.quantity
       },
       0
@@ -25,17 +32,21 @@ const sumAssembliesPrice = (
 }
 
 const Price: FC = () => {
+  const handles = useCssHandles(CSS_HANDLES)
   const { price, children } = useProductAssemblyItem() as AssemblyItem
 
   if ((price == null || price === 0) && !children) {
     return null
   }
+
   const childrenCost = children ? sumAssembliesPrice(children) : 0
   const totalPrice = (price + childrenCost) / 100
 
   return (
-    <div className="flex items-center t-body b c-on-base">
-      <span>+ </span>
+    <div
+      className={`${handles.productAssemblyOptionItemPrice} flex items-center t-body b c-on-base`}
+    >
+      <span className={handles.productAssemblyOptionItemSign}>+ </span>
       <ProductPrice
         showLabels={false}
         showListPrice={false}
