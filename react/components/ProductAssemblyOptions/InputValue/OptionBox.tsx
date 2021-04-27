c5a74bec22c479e220ebf0bd4ee2e82536b95c6e
Gabi Danciu <gabi.danciu@webtoclick.com>
2021-04-27 16:55:15 +0300

    adding handles to option box

--- react/components/ProductAssemblyOptions/InputValue/OptionBox.tsx
+++ react/components/ProductAssemblyOptions/InputValue/OptionBox.tsx
@@ -5,7 +5,7 @@ import { useCssHandles, applyModifiers } from 'vtex.css-handles'
 import slugify from '../../../modules/slugify'
 import styles from '../styles.css'
 
-const CSS_HANDLES = ['inputValueOptionBox'] as const
+const CSS_HANDLES = ['inputValueOptionBox', 'inputValueOptionBoxItem', 'inputValueOptionBoxItemActive'] as const
 
 const OptionBox: FC<Props> = ({ option, selected, onClick, onKeyDown }) => {
   const handles = useCssHandles(CSS_HANDLES)
@@ -32,6 +32,8 @@ const OptionBox: FC<Props> = ({ option, selected, onClick, onKeyDown }) => {
       />
       <div
         className={classNames(
+          handles.inputValueOptionBoxItem,
+          selected ? handles.inputValueOptionBoxItemActive : '' ,
           'w-100 h-100 ba br2 bw1 b--muted-4 z-1 c-muted-5 flex items-center overflow-hidden',
           {
             'hover-b--muted-2': !selected,
